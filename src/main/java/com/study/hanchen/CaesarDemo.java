310fce5a313b2c4de5a51204c152a1737b75f708
jackhanchen123 <846069304@qq.com>
2021-08-10 01:02:25 +0800

    DES加解密

--- src/main/java/com/study/hanchen/CaesarDemo.java
+++ src/main/java/com/study/hanchen/CaesarDemo.java
@@ -1,15 +1,74 @@
 package com.study.hanchen;
 
 
+import java.nio.charset.StandardCharsets;
+
 /**
- * 凯撒密码加密  统一向右移动进行加密
- *
+ * 凯撒密码加密  统一向右移动进行加密 同一把key加解密--*--->>>>对称加密算法（DES AES算法）
+ * <p>
  * encrypt 加密
  * decrypt 解密
  */
 public class CaesarDemo {
 
     public static void main(String[] args) {
+        //1.明文
+        String plainText = "i love you";
+        //2.加密规则将字母向右移动3位
+        int key = 3;
+        //3.加密
+        String cipher = encrypt(plainText, key);
+        System.out.println(cipher);
 
+        //4.解密
+        String decrypt=decrypt(cipher,key);
+        System.out.println(decrypt);
     }
+
+    /**
+     * 解密
+     * @param cipher
+     * @param key
+     * @return
+     */
+    private static String decrypt(String cipher, int key) {
+        char[] chars = cipher.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            chars[i]= (char) (chars[i]-3);
+        }
+        return String.valueOf(chars);
+    }
+
+    /**
+     * 加密方法
+     *
+     * @param plainText
+     * @param key
+     * @return
+     */
+    private static String encrypt(String plainText, int key) {
+//        byte[] bytes = plainText.getBytes(StandardCharsets.UTF_8);
+//        for (byte b:bytes){
+//            b=(byte) (b+key);
+//        }
+//
+//        return bytes.toString();
+
+ //string char byte 关系以及转换
+
+        char[] chars = plainText.toCharArray();
+//        for (char c : chars) {
+//            c = (char) (c + key);
+//        }
+        for (int i = 0; i < chars.length; i++) {
+            //ASCII码表：a(97)  b(98)  可直接加
+            chars[i]= (char) (chars[i]+key);
+        }
+        return new String(chars);
+    }
+
+
+
+
 }
--- src/main/java/com/study/hanchen/DESDemo.java
+++ src/main/java/com/study/hanchen/DESDemo.java
@@ -19,22 +19,52 @@ import java.util.Base64;
 public class DESDemo {
     public static void main(String[] args) throws Exception {
 
-        String clearText="hanchen"; //明文
-
-        String originKey ="12345678";//秘钥
-
+        String clearText="hanchen"; //明文plainText
+        String originKey ="12345678";//秘钥key
+        //DES算法
         String cipherText =desEncrpt(clearText,originKey);
 
         System.out.println(cipherText);
+
+        //解密
+        String plainText=  desDecrypt(cipherText,originKey);
+        System.out.println(plainText);
     }
 
+    /**
+     * 解密
+     * @param cipherText
+     * @param originKey
+     * @return
+     */
+    private static String desDecrypt(String cipherText, String originKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher des = Cipher.getInstance("des");
+        Key key = getKey(originKey);
+        des.init(Cipher.DECRYPT_MODE,key);
+        byte[] decode = Base64.getDecoder().decode(cipherText);
+        byte[] bytes = des.doFinal(decode);
+        return new String(bytes);
+    }
+
+    /**
+     * 都是对byte进行一些数学运算
+     * @param clearText
+     * @param originKey
+     * @return
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
     private static String desEncrpt(String clearText, String originKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        //1.实例化des加密算法工具类
+        //1.Cipher--->>>实例化des加密算法工具类//Algorithm算法
         Cipher cipher =Cipher.getInstance("des");
-        //int opmode 加密/解密模式
-        //Key key 对原始秘钥处理之后的秘钥
+        //原始秘钥转换成key
         Key key=getKey(originKey);
         //2.对工具类对象初始化
+        // int opmode 加密/解密模式
+        // Key key 对原始秘钥处理之后的秘钥
         cipher.init(Cipher.ENCRYPT_MODE,  key);
 
         //3.用加密工具类对象cipher开始加密-》密文
